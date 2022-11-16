48852bcb7ed2abc6ee537db64e28b840dabe3e21
Leo <lclipet@gaming.tech>
2022-11-16 08:46:12 +0100

    final-test

--- pierre-feuille-ciseau.py
+++ pierre-feuille-ciseau.py
@@ -5,13 +5,13 @@ import random
 #on admet input une fonction qui renvoie la valeur qu'a donner le joueur
 
 def coup(num):
-    if num == 1:
+    if num == "1":
         #définir pierre = 1
         return "pierre"
-    if num == 2:
+    if num == "2":
         #définir feuille = 2
         return "feuille"
-    if num == 3:
+    if num == "3":
         #définir ciseau = 3
         return "ciseau"
 
@@ -19,19 +19,25 @@ def coup(num):
 def choixBot():
     #définir une variable randomNb prend pour valeur le retour de l'execution de la fonction random(1, 3)
     randomNb = random.randint(1, 3)
+    str(randomNb)
     #retourner randomNb
     return randomNb
 
 #définir une fonction choixJoueur
 def choixJoueur():
-    #définir une variable choicePl qui prend pour valeur le retour de l'execution de la fonction input(1, 3)
-    choiceP1 = int(input("Quel coup veut-tu jouer : Pierre 1, Feuille 2 ou Ciseau 3 ? "))
-    #retourner la valeur
-    return choiceP1
+    #définir une variable choiceP1 qui prend pour valeur le retour de l'execution de la fonction input(1, 3)
+    choiceP1 = input("Quel coup veut-tu jouer : Pierre 1, Feuille 2 ou Ciseau 3 ? ")
+    if choiceP1 == "1" or "2" or "3":
+        #retourner la valeur
+        return choiceP1
+    else:
+        print("La valeur n'est pas valide, veullez entrer 1, 2 ou 3")
+    
 
 
 #définir une fonction resultat
 def resultat():
+    result = ""
     #définir coupJoueur une variable qui est égale à la fonction choixJoueur()
     coupJoueur = choixJoueur()
     turnJoueur = coup(coupJoueur)
@@ -41,13 +47,13 @@ def resultat():
     turnBot = coup(coupBot)
     print("Le bot a joué ", coupBot, ", c'est à dire : ", turnBot)
     #si choixJoueur == 1 alors
-    if coupJoueur == 1:
+    if coupJoueur == "1":
         #si choixBot == 1
-        if coupBot == 1:
+        if coupBot == "1":
             #alors afficher "égalité"
             result = "Egalité"
         #si choixBot == 2
-        elif coupBot == 2:
+        elif coupBot == "2":
             #alors afficher "perdu"
             result = "Perdu"
         #si choixBot == 3
@@ -55,13 +61,13 @@ def resultat():
             #alors afficher "gagné"
             result = "Gagné"
     #si choixJoueur == 2 alors
-    if coupJoueur == 2:
+    elif coupJoueur == "2":
         #si choixBot == 1
-        if coupBot == 1:
+        if coupBot == "1":
             #alors afficher "gagné"
             result = "Gagné"
         #si choixBot == 2
-        elif coupBot == 2:
+        elif coupBot == "2":
             #alors afficher "égalité"
             result = "Egalité"
         #si choixBot == 3
@@ -69,13 +75,13 @@ def resultat():
             #alors afficher "perdu"
             result = "Perdu"
     #si choixJoueur == 3 alors
-    if coupJoueur == 3:
+    elif coupJoueur == "3":
         #si choixBot == 1
-        if coupBot == 1:
+        if coupBot == "1":
             #alors afficher "perdu"
             result = "Perdu"
         #si choixBot == 2
-        elif coupBot == 2:
+        elif coupBot == "2":
             #alors afficher "gagné"
             result = "Gagné"
         #si choixBot == 3
