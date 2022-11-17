2493b5c8c9884f9096ad19e1d201c94e819a0d97
Leo <lclipet@gaming.tech>
2022-11-17 11:45:47 +0100

    initialize repository

--- morpion-pseudo-code.py
+++ morpion-pseudo-code.py
@@ -1,8 +1,48 @@
 #DEBUT
 
-#importer/faire un tableau
-#importer/faire une interface graphique 
+#définir une fonction afficher_grille() avec comme paramètre grille
+    #afficher le numéro de chaques colonnes
+    #afficher la permière ligne
+    #afficher le numéro de la première ligne
+    #afficher chaques colonnes de la ligne
+    #afficher la seconde ligne
+    #afficher le numéro de la seconde ligne
+    #afficher chaques colonnes de la ligne
+    #afficher la troisieme ligne
+    #afficher le numéro de la troisième ligne
+    #afficher chaques colonnes de la ligne
+    #afficher la dernière ligne
 
-#
+#définir une fonction tour() avec comme paramètres grille et joueur
+    #afficher "C'est à toi joueur", le numéro du joueur
+    #définir la variable colonne comme input qui affiche "Le numero de colonne que tu veux jouer : "
+    #définir la variable ligne comme input qui affiche "Et maintenant la ligne : "
+    #afficher "OK ! C'est placé dans la case ("+numéro de la colonne+","+numéro de la ligne+")"
+    #faire une boucle tant que la case selectionné est prise
+        #appeler la fonction afficher_grille()
+        #afficher "Cette case est déjà prise >:( selectionne un autre case !"
+        #définir la variable colonne comme input qui affiche "Le numero de colonne que tu veux jouer : "
+        #définir la variable ligne comme input qui affiche "Et maintenant la ligne : "
+        #afficher "OK ! C'est placé dans la case ("+numéro de la colonne+","+numéro de la ligne+")"
+    
+    #si la variable joueur est égale à 1
+        #alors afficher "X" dans la case selectionner
+    ##si la variable joueur est égale à 2
+        #alors afficher O dans la case selectionner
+    #appeler la fonction afficher_grille()
+
+#définir une fonction gagnant() avec comme paramètre grille
+    #si case(...) est prise ET case(...) est prise ET case(...) est différente de " "
+        #alors retourner la valeur 1
+    #repeter la boucle du dessu 8 fois (le nombre de possibilité de gagner au morpion)
+
+#définir une fonction match_nul() avec comme paramètre grille
+    #pour i le nombre de case sur la grille allant jusqu'à 9
+        #si la valeur grille ayant comme paramètre i est égale à " " (c'est à dire qu'aucune ligne n'a été former par un joueur)
+            #alors retourner la valeur 0
+    #retourner la valeur 1
+
+
+
 
 #FIN
