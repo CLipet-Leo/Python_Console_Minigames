31062d3d49e6308f63cbebc87d5ed16693a6dc88
Leo <lclipet@gaming.tech>
2022-11-23 09:35:26 +0100

    final python pfc

--- pierre-feuille-ciseau-python.py
+++ pierre-feuille-ciseau-python.py
@@ -18,7 +18,7 @@ def coup(num):
     else:
         return "Mauvaise saisie"
 
-def resultat():
+def contreBot():
     scoreP = 0
     scoreB = 0
     while scoreP or scoreB < 3:
@@ -88,8 +88,87 @@ def resultat():
             break
         elif scoreB == 3:
             print("Victoire du Bot !!!! T nul")
-            break      
+            break
 
-# appeler la fonction resultat()
-resultat()
+def contreJ():
+    scoreP = 0
+    scoreB = 0
+    while scoreP or scoreB < 3:
+        # définir coupJoueur une variable qui est égale à la fonction choixJoueur()
+        coupJoueur1 = int(input("Quel coup veut-tu jouer joueur1 : Pierre 1, Feuille 2 ou Ciseau 3 ? ", ))
+        turnJoueur1 = coup(coupJoueur1)
+        print("Joueur 1 tu as joué ", turnJoueur1)
+        # définir coupBot une variable qui est égale à la fonction choixBot()
+        coupJoueur2 = int(input("Quel coup veut-tu jouer joueur2 : Pierre 1, Feuille 2 ou Ciseau 3 ? ", ))
+        turnJoueur2 = coup(coupJoueur2)
+        print("Joueur 2 tu as joué ", turnJoueur2)
+        # si choixJoueur == 1 alors
+        if coupJoueur1 == 1:
+            # si choixBot == 1
+            if coupJoueur2 == 1:
+                # alors afficher "égalité"
+                print("Egalité")
+            # si choixBot == 2
+            elif coupJoueur2 == 2:
+                # alors afficher "perdu"
+                scoreB = scoreB + 1
+            # si choixBot == 3
+            else:
+                # alors afficher "gagné"
+                scoreP = scoreP + 1
+        # si choixJoueur == 2 alors
+        elif coupJoueur1 == 2:
+            # si choixBot == 1
+            if coupJoueur2 == 1:
+                # alors afficher "gagné"
+                scoreP = scoreP + 1
+            # si choixBot == 2
+            elif coupJoueur2 == 2:
+                # alors afficher "égalité"
+                print("Egalité")
+            # si choixBot == 3
+            else:
+                # alors afficher "perdu"
+                scoreB = scoreB + 1
+        # si choixJoueur == 3 alors
+        elif coupJoueur1 == 3:
+            # si choixBot == 1
+            if coupJoueur2 == 1:
+                # alors afficher "perdu"
+                scoreB = scoreB + 1
+            # si choixBot == 2
+            elif coupJoueur2 == 2:
+                # alors afficher "gagné"
+                scoreP = scoreP + 1
+            # si choixBot == 3
+            else:
+                # alors afficher "égalité"
+                print("Egalité")
+        else:
+            print("Votre choix n'est pas correct, vérifiez la valeur")
+        
+        print("Score du joueur 1 : ", scoreP)
+        print("Score du joueur 2 : ", scoreB)
+        if scoreP == 3:
+            print("Victoire du Joueur 1")
+            break
+        elif scoreB == 3:
+            print("Victoire du Joueur 2")
+            break
+
+# appeler la fonction resultat
+r=0
+while r==0:
+    modeDeJeu=int(input("Veut-tu jouer contre un Bot ?(entre 1) ou contre un humain ?(entre 2) : "))
+    if modeDeJeu == 1:
+        contreBot()
+        r=r+1
+        break
+    elif modeDeJeu == 2:
+        contreJ()
+        r=r+1
+        break
+    else:
+        print("valeur incorrecte")
+
 # FIN
